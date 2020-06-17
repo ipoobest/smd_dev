88dc705f246d8e379e40b4d9f20e87de565e5746
mr hoikhong <masterpoohh@gmail.com>
2020-06-17 14:03:16 +0700

    get teach by teacher

--- pages/teachers/teach/subjects.vue
+++ pages/teachers/teach/subjects.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>{{ params }}</h3>
+    <h3>{{ teachObjectId }}</h3>
   </div>
 </template>
 
@@ -9,13 +9,15 @@
     layout: 'teacher',
     mounted () {
       this.params = this.$route.params
+      this.teachObjectId = this.$store.state.auth.auth.teacherObjectId
       // conslode.log('route params', this.$route.query)
-      console.log(this.$route.params)
-      this.getTeachByTeacherId(this.$route.params.id).then(result => (this.items = result))
+      console.log(this.teachObjectId)
+      this.getTeachByTeacherId(this.teachObjectId).then(result => (this.items = result))
     },
     data() {
       return {
         params: '',
+        teachObjectId: '',
         items: []
       }
     },
@@ -24,7 +26,7 @@
        const data = {
            'schoolYear': this.$route.params.schoolYear,
            'term': this.$route.params.term,
-           'teacher.value': 'q5BHP59DvT'
+           'teacher.value': teacherId
         }
         const response = await this.$store.dispatch(`teach/getTeachByTeacherId`, data )
         console.log('response', response)
