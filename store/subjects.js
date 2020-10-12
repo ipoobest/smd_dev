e5a3a1d63e346991ff1614a41e4b0bdf661e88ec
Thanachai Puttiporn (Dome Cloud) <poobest.pooh@gmail.com>
2020-10-12 11:46:20 +0700

    change limit getSubjectByConditions

--- store/subjects.js
+++ store/subjects.js
@@ -70,7 +70,7 @@ export const actions = {
       try {
         const where = JSON.stringify(conditions)
         console.log('where', where)
-        const result = this.$axios.$get(`${process.env.parseUrl}/classes/${classes.subjects}?where=${where}`)
+        const result = this.$axios.$get(`${process.env.parseUrl}/classes/${classes.subjects}?where=${where}&&limit=500`)
 
         commit("setSubjects", result);
         resolve(result)
