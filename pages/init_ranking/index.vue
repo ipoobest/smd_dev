b99210490ca2f2ef7e5a87e2cda5ff189cc3258b
Thanachai Puttiporn (Dome Cloud) <poobest.pooh@gmail.com>
2020-11-25 12:02:17 +0700

    init ranking /init_ranking

---
+++ pages/init_ranking/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    init data
+    <v-row justify="center">
+      <v-col cols="3">
+        <v-select
+          v-model="classes"
+          :items="items"
+          label="Standard"
+          @change="getStudent()"
+        ></v-select>
+        </v-col>
+        
+    </v-row>
+    <v-row justify="center">
+      <v-btn @click="create" class="success">init</v-btn>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "blank",
+  async mounted() {
+    await this.getStudent().then(result => {
+      this.students = result;
+    });
+  },
+  data() {
+    return {
+      students: [],
+      classes: '',
+      items: [
+        "ม.1",
+        "ม.2",
+        "ม.3",
+        "ม.4",
+        "ม.5",
+        "ม.6",
+      ]
+    };
+  },
+  // 1 get student
+  methods: {
+    async getStudent() {
+      var query = {
+        schoolYear: "2563",
+        term: "1",
+        classRoomLevel: this.classes
+      };
+      const response = await this.$store.dispatch(
+        "grade/getGradeByConditions",
+        query
+      );
+      // console.log('response.results', response.results)
+      var students = response.results;
+      var resArr = [];
+      students.filter(function(item) {
+        var i = resArr.findIndex(x => x.studentId == item.studentId);
+        if (i <= -1) {
+          resArr.push({ studentId: item.studentId, schoolYear: item.schoolYear, term: item.term, gpa: 0, rankingInRoom: 0, rankingInClasses: 0, classRoomLevel: item.classRoomLevel,});
+        }
+        return null;
+      });
+      // xxx = response.results[0].filter((x, i, a) => a.indexOf(x) == i)
+      console.log("response.results", resArr);
+      // ar unique = a.filter( onlyUnique );
+      this.students = resArr
+      // return resArr
+    },
+    async createRanking(data) {
+      var response = await this.$store.dispatch(
+        `ranking/createRanking`,
+        data
+      );
+      console.log('respose create success', response)
+    },
+    async create() {
+      console.log('xxxx')
+      this.students.forEach(item => {
+        this.createRanking(item)
+        // console.log('item : ', item.studentId)
+      })
+      alert('init success')
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- store/grade.js
+++ store/grade.js
@@ -1,125 +1,125 @@
-import classes from '~/utils/classes'
+import classes from "~/utils/classes";
 
 export const state = () => {
   return {
     grade: null,
     error: null
-  }
-}
+  };
+};
 
 export const mutations = {
-  setGrade (state, data) {
-    state.grade = data
+  setGrade(state, data) {
+    state.grade = data;
   },
-  setError (state, error) {
-    state.error = error
+  setError(state, error) {
+    state.error = error;
   }
-}
+};
 
 export const actions = {
-         async createGrade({ commit }, object) {
-           return new Promise(async (resolve, reject) => {
-             try {
-               const result = this.$axios.$post(
-                 `${process.env.parseUrl}/classes/${classes.grade}`,
-                 object
-               );
+  async createGrade({ commit }, object) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = this.$axios.$post(
+          `${process.env.parseUrl}/classes/${classes.grade}`,
+          object
+        );
 
-               commit("setGrade", result);
-               resolve(result);
-             } catch (error) {
-               commit(`setError`, error);
-               reject(error);
-             }
-           });
-         },
-         async getGrade({ commit }) {
-           return new Promise(async (resolve, reject) => {
-             try {
-               const result = this.$axios.$get(
-                 `${process.env.parseUrl}/classes/${classes.grade}`
-               );
+        commit("setGrade", result);
+        resolve(result);
+      } catch (error) {
+        commit(`setError`, error);
+        reject(error);
+      }
+    });
+  },
+  async getGrade({ commit }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = this.$axios.$get(
+          `${process.env.parseUrl}/classes/${classes.grade}`
+        );
 
-               commit("setGrade", result);
-               resolve(result);
-             } catch (error) {
-               console.log({ error });
-               commit(`setError`, error);
-               reject(error);
-             }
-           });
-         },
-         async getGradeByConditions({ commit }, conditions) {
-           return new Promise(async (resolve, reject) => {
-             try {
-               const where = JSON.stringify(conditions);
-               const result = await this.$axios.$get(
-                 `${process.env.parseUrl}/classes/${classes.grade}?where=${where}&&limit=500`
-               );
-               console.log(
-                 "grades url",
-                 `${process.env.parseUrl}/classes/${classes.grade}?where=${where}`
-               );
-               // commit('setGrade', result)
-               resolve(result);
-             } catch (error) {
-               console.log({ error });
-               commit(`setError`, error);
-               reject(error);
-             }
-           });
-         },
-         async updateGrade({ commit }, object) {
-           return new Promise(async (resolve, reject) => {
-             const objectId = object.objectId;
-             console.log("grades objecId", objectId);
-             try {
-               const result = await this.$axios.$put(
-                 `${process.env.parseUrl}/classes/${classes.grade}/${objectId}`,
-                 object
-               );
+        commit("setGrade", result);
+        resolve(result);
+      } catch (error) {
+        console.log({ error });
+        commit(`setError`, error);
+        reject(error);
+      }
+    });
+  },
+  async getGradeByConditions({ commit }, conditions) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const where = JSON.stringify(conditions);
+        const result = await this.$axios.$get(
+          `${process.env.parseUrl}/classes/${classes.grade}?where=${where}&&limit=2000`
+        );
+        console.log(
+          "grades url",
+          `${process.env.parseUrl}/classes/${classes.grade}?where=${where}`
+        );
+        // commit('setGrade', result)
+        resolve(result);
+      } catch (error) {
+        console.log({ error });
+        commit(`setError`, error);
+        reject(error);
+      }
+    });
+  },
+  async updateGrade({ commit }, object) {
+    return new Promise(async (resolve, reject) => {
+      const objectId = object.objectId;
+      console.log("grades objecId", objectId);
+      try {
+        const result = await this.$axios.$put(
+          `${process.env.parseUrl}/classes/${classes.grade}/${objectId}`,
+          object
+        );
 
-               commit("setGrade", result);
-               resolve(result);
-             } catch (error) {
-               console.log({ error });
-               commit(`setError`, error);
-               reject(error);
-             }
-           });
-         },
-         async deleteGrade({ commit }, objectId) {
-           return new Promise(async (resolve, reject) => {
-             try {
-               console.log("delete id", objectId);
-               const result = this.$axios.$delete(
-                 `${process.env.parseUrl}/classes/${classes.grade}/${objectId}`
-               );
+        commit("setGrade", result);
+        resolve(result);
+      } catch (error) {
+        console.log({ error });
+        commit(`setError`, error);
+        reject(error);
+      }
+    });
+  },
+  async deleteGrade({ commit }, objectId) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        console.log("delete id", objectId);
+        const result = this.$axios.$delete(
+          `${process.env.parseUrl}/classes/${classes.grade}/${objectId}`
+        );
 
-               commit("setGrade", null);
-               resolve(result);
-             } catch (error) {
-               console.log({ error });
-               commit(`setError`, error);
-               reject(error);
-             }
-           });
-         },
-         async deleteGradeByConditions({ commit }, conditions) {
-           return new Promise(async (resolve, reject) => {
-             try {
-               const where = JSON.stringify(conditions);
-               const result = this.$axios.$delete(
-                 `${process.env.parseUrl}/classes/${classes.grade}${classes.grade}?where=${where}`
-               );
+        commit("setGrade", null);
+        resolve(result);
+      } catch (error) {
+        console.log({ error });
+        commit(`setError`, error);
+        reject(error);
+      }
+    });
+  },
+  async deleteGradeByConditions({ commit }, conditions) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const where = JSON.stringify(conditions);
+        const result = this.$axios.$delete(
+          `${process.env.parseUrl}/classes/${classes.grade}${classes.grade}?where=${where}`
+        );
 
-               commit("setGrade", null);
-               resolve(result);
-             } catch (error) {
-               console.log({ error });
-               commit(`setError`, error);
-               reject(error);
-             }
-           });
-         }
-       };
+        commit("setGrade", null);
+        resolve(result);
+      } catch (error) {
+        console.log({ error });
+        commit(`setError`, error);
+        reject(error);
+      }
+    });
+  }
+};
